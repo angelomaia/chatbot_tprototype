e753f813a5efe8d1ea0be3aa0d27578dd2f5347d
Angelo Maia <request.angelo@gmail.com>
2023-02-13 13:50:04 -0300

    New temperature

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def chatkbt(txt, history):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        temperature=0.5,
+        temperature=0.6,
         max_tokens=150,
         top_p=1,
         frequency_penalty=0,
